d64ee445b70a89918a3cb991bf208713e4961ec4
seif <saly@edu.hse.ru>
2023-09-13 16:27:57 +0300

    Initial Commit

---
+++ salyPW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  salyPW1
+//
+//  Created by seif on 2023-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
